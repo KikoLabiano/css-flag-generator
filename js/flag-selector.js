fe950f1c181f116af1a5e52152c7c341c2034291
d807354 <fj.labiano.jauregui@navarra.es>
2018-10-18 16:25:00 +0200

    html2canvas + module pattern

--- js/flag-selector.js
+++ js/flag-selector.js
@@ -1,10 +1,21 @@
-function changeFlag(flag_class){
-  let f = document.getElementById("flag");
-  f.className='';
-  //tr td specifity from .flag class is greater than .bhu-flag class
-  if(flag_class!=="bhu-flag"){    
-    f.classList.add("flag-clean");
+var flagGenerator = (function () {
+  var publicAPI = {
+    changeFlag: (flag_class) => {
+      let f = document.getElementById("flag");
+      f.className = '';
+      //tr td specifity from .flag class is greater than .bhu-flag class
+      if (flag_class !== "bhu-flag") {
+        f.classList.add("flag-clean");
+      }
+      f.classList.add("flag-border");
+      f.classList.add(flag_class);
+    },
+    exportPNG:()=>{   
+      html2canvas(document.querySelector("#flag")).then(canvas => {
+      //document.body.appendChild(canvas);
+      });}
   }
-  f.classList.add("flag-border");
-  f.classList.add(flag_class);
-}
+  return publicAPI;
+})();
+
+
