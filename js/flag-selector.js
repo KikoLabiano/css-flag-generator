ab1ca55dd18b4d2d54d5459542af6333deabfc26
d807354 <fj.labiano.jauregui@navarra.es>
2018-10-18 12:41:46 +0200

    Cambio

--- js/flag-selector.js
+++ js/flag-selector.js
@@ -2,8 +2,7 @@ function changeFlag(flag_class){
   let f = document.getElementById("flag");
   f.className='';
   //tr td specifity from .flag class is greater than .bhu-flag class
-  if(flag_class!=="bhu-flag")
-  {    
+  if(flag_class!=="bhu-flag"){    
     f.classList.add("flag-clean");
   }
   f.classList.add("flag-border");
