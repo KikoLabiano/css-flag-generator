d48318a05fbaa95465a62a057dab11a0cac7a1bf
d807354 <fj.labiano.jauregui@navarra.es>
2018-10-18 12:40:06 +0200

    Más banderas

--- js/flag-selector.js
+++ js/flag-selector.js
@@ -1,6 +1,11 @@
 function changeFlag(flag_class){
   let f = document.getElementById("flag");
   f.className='';
-  f.classList.add("flag");
+  //tr td specifity from .flag class is greater than .bhu-flag class
+  if(flag_class!=="bhu-flag")
+  {    
+    f.classList.add("flag-clean");
+  }
+  f.classList.add("flag-border");
   f.classList.add(flag_class);
 }
